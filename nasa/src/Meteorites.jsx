0021512c9d394a49394da77d72d689c1b4630acb
cheesenbeans <chrishorsley1@hotmail.co.uk>
2023-05-22 17:02:41 +0100

    this commit adds the map for selected Meteor but not the marker on the map , grrrrr

--- nasa/src/Meteorites.jsx
+++ nasa/src/Meteorites.jsx
@@ -1,8 +1,10 @@
-
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import {Icon} from 'leaflet'
+import 'leaflet/dist/leaflet.css';
+import './App.css' 
 
 export default function Meteorites({name, mass, year, geolocation}) {
 
-    
     return (
         <li>
             <p>Name: {name}</p>
@@ -10,6 +12,17 @@ export default function Meteorites({name, mass, year, geolocation}) {
             <p>Year: {year ? year : "Data missing"}</p>
             <p>Latitude: {geolocation ? geolocation.latitude : ""}</p>
             <p>Longitude: {geolocation ? geolocation.longitude : "has not fallen"}</p>
+            <MapContainer className="map" center={[geolocation.latitude, geolocation.longitude]} zoom={10} scrollWheelZoom={false}>
+                <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={[geolocation.longitude, geolocation.latitude]}>
+                    <Popup>
+
+                    </Popup>
+                </Marker>
+            </MapContainer>
         </li>
     )
 }
