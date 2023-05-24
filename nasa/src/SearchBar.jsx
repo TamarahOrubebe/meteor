a3c9df44b6320482be4808bc53926ca531e03412
Orubebe Tamarah <paulorubebe@yahoo.com>
2023-05-24 21:34:40 +0100

    added a public folder to serve static files

--- nasa/src/SearchBar.jsx
+++ nasa/src/SearchBar.jsx
@@ -24,7 +24,7 @@ export default function SearchBar({ setSearchTerm }) {
   return (
       <section className="searchBar">
         <div className="nasa-header">
-        <img className="nasa" src="../src/images/NASA_logo.png"/>
+        <img className="nasa" src="/images/NASA_logo.png"/>
         <h1>NASA Meteorite Landings</h1>
         </div>
         <h3>Search by name</h3>
